3fdef1bd965db84fd22bfdaa727b7904a25e2c34
thdecn <60790384+thdecn@users.noreply.github.com>
2020-12-30 20:25:56 +0100

    Create lwe_operations.py
    Added code for homomorphic addition and subtraction of ciphertexts

---
+++ lwe_operations.py
@@ -0,0 +1,172 @@
+
+#----Functions for Homomorphic Addition and Subtraction-------------------------
+def nb_bit_from_variance_99(var, torus_bits):
+    """Computes the number of bits affected by normal distributed noise with a Variance var.
+        Takes into account the number of bits of the integers.
+    Args:
+        var: Variance of the normal distribution.
+        torus_bits: from q=2^torus_bits.
+    Returns:
+        Number of affected bits.
+    """
+    # Compute std_dev
+    sigma = np.sqrt(var)
+    # Constant to get 99% of the normal distribution
+    z = 3.0
+    tmp = np.float64(torus_bits) + np.float64(np.log2(sigma * z))
+    if tmp < 0.0:
+        # Means no bits are affected by the noise in the integer representation (discrete space)
+        nr_bit = 0
+    else:
+        nr_bit = np.ceil(tmp)
+
+    return nr_bit
+
+def update_precision_from_variance(var, enc, q):
+    """After an homomorphic operation, update an encoder using the new variance.
+    Args:
+        var: new variance.
+        enc: Encoder to be updated [min, delta, nb_bit_precision, nb_bit_padding, round].
+        q: ciphertext modulus 2^torus_bits.
+    Returns:
+        Returns the number of bits of precision affected by the noise.
+    Errors:
+        NoNoiseInCiphertext: If there is no noise in the ciphertext.
+    """
+    torus_bits = np.int64(np.log2(q))
+    # Check output noise
+    nb_noise_bits = nb_bit_from_variance_99(var, torus_bits)
+
+    new_precision = np.uint64(0)
+    # Check if there is noise in the ciphertext
+    if (nb_noise_bits == 0):
+        print("NoNoiseInCiphertext")
+    elif ( nb_noise_bits + enc[2] + enc[3] > torus_bits ):
+        # Compute the number of bits which can be overwritten by the noise
+        nb_bit_overlap = nb_noise_bits + enc[2] + enc[3] - torus_bits
+        # If the overlap is at least as big as the precision, there is no more message
+        new_precision = np.maximum(enc[2] - nb_bit_overlap, 0)
+
+    return new_precision
+
+def lwe_addition(ct1, var1, enc1, ct2, var2, enc2, exact, q):
+    """Compute an addition between two LWE ciphertexts by eating one bit of padding.
+        Corresponds to "add_with_padding_{exact}" from the Rust implementation.
+    Args:
+        ct1, ct2: Ciphertexts to be added.
+        enc1, enc2: Corresponding encoder values [min, delta, nb_bit_precision, nb_bit_padding].
+        var1, var2: Corresponding variances.
+        exact: exact=1 ? (message bits increase max(nb1,nb2) + 1) :
+                            (message bits stay the same min(nb1,nb2)).
+        q: ciphertext modulus 2^torus_bits.
+    Returns:
+        Ciphertext ct3.
+        Corresponding Variance var3.
+        Corresponding Encoder enc3.
+    Errors:
+        DeltaError: The encoders have incompatible deltas.
+        PaddingError: The encoders have incompatible paddings.
+        NotEnoughPaddingError: Number of padding bits is zero.
+        DimensionError: The ciphertexts have incompatible dimensions.
+    """
+    # Checks
+    tmp = np.abs(enc1[1] - enc2[1]) / np.maximum(enc1[1], enc2[1])
+    limit1 = enc1[1] / pow(2.0, -40)
+    limit2 = enc2[1] / pow(2.0, -40)
+    if ( not (tmp < limit1 and tmp < limit2) ):
+        print("DeltaError")
+    if (enc1[3] != enc2[3]):
+        print("PaddingError")
+    if (len(ct1) != len(ct2)):
+        print("DimensionError")
+    # At least one bit of padding needed
+    if (enc1[3] == 0):
+        print("NotEnoughPaddingError")
+
+    # Add ciphertexts together
+    ct3 = np.uint64(np.zeros(len(ct1)))
+    for j in range(len(ct1)):
+        ct3[j] = (ct1[j] + ct2[j]) % q
+
+    # Update the variances and the Encoder
+    # Compute the new variance
+    var3 = var1 + var2
+
+    # Compute the new encoder
+    enc3 = np.zeros(len(enc1))
+    enc3[0] = enc1[0] + enc2[0]
+    enc3[1] = enc1[1] * 2.0
+    if (exact == 0):
+        enc3[2] = np.minimum(enc1[2], enc2[2])
+    else:
+        enc3[2] = np.maximum(enc1[2], enc2[2]) + 1
+    enc3[3] = enc1[3] - 1
+
+    # Update the encoder precision based on the variance
+    # __TODO__ enc3[2] = update_precision_from_variance(var3, enc3, q)
+
+    return ct3, var3, enc3
+
+def lwe_subtraction(ct1, var1, enc1, ct2, var2, enc2, exact, q):
+    """Compute a subtraction between two LWE ciphertexts by eating one bit of padding.
+        Corresponds to "sub_with_padding_{exact}" from the Rust implementation.
+    Args:
+        ct1, ct2: Ciphertexts to be subtracted.
+        enc1, enc2: Corresponding encoder values [min, delta, nb_bit_precision, nb_bit_padding].
+        var1, var2: Corresponding variances.
+        exact: exact=1 ? (message bits increase max(nb1,nb2) + 1) :
+                            (message bits stay the same min(nb1,nb2)).
+        q: ciphertext modulus 2^torus_bits.
+    Returns:
+        Ciphertext ct3.
+        Corresponding Variance var3.
+        Corresponding Encoder enc3.
+    Errors:
+        DeltaError: The encoders have incompatible deltas.
+        PaddingError: The encoders have incompatible paddings.
+        NotEnoughPaddingError: Number of padding bits is zero.
+        DimensionError: The ciphertexts have incompatible dimensions.
+    """
+    # Checks
+    tmp = np.abs(enc1[1] - enc2[1]) / np.maximum(enc1[1], enc2[1])
+    limit1 = enc1[1] / pow(2.0, -40)
+    limit2 = enc2[1] / pow(2.0, -40)
+    if ( not (tmp < limit1 and tmp < limit2) ):
+        print("DeltaError")
+    if (enc1[3] != enc2[3]):
+        print("PaddingError")
+    if (len(ct1) != len(ct2)):
+        print("DimensionError")
+    # At least one bit of padding needed
+    if (enc1[3] == 0):
+        print("NotEnoughPaddingError")
+
+    # Subtract ciphertexts
+    ct3 = np.uint64(np.zeros(len(ct1)))
+    for j in range(len(ct1)):
+        ct3[j] = np.uint64((np.int64(ct1[j]) - np.int64(ct2[j])) % q)
+
+    # Correction related to the subtraction
+    correction = 1 << np.int(np.log2(q) - enc1[3])
+    ct3[-1] = (ct3[-1] + np.uint64(correction)) % q
+
+    # Update the variances and the Encoder
+    # Compute the new variance
+    var3 = var1 + var2
+
+    # Compute the new encoder
+    enc3 = np.zeros(len(enc1))
+    enc3[0] = enc1[0] - (enc2[0] + enc2[1])
+    enc3[1] = enc1[1] * 2.0
+    if (exact == 0):
+        enc3[2] = np.minimum(enc1[2], enc2[2])
+    else:
+        enc3[2] = np.maximum(enc1[2], enc2[2]) + 1
+    enc3[3] = enc1[3] - 1
+
+    # Update the encoder precision based on the variance
+    # __TODO__ enc3[2] = update_precision_from_variance(var3, enc3, q)
+
+    return ct3, var3, enc3
+
+#===============================================================================
